3ae2d9e6c69f2775dcfa29dc4b72a929db9b387a
z9956 <zhz995617@gmail.com>
2021-09-28 17:10:59 +0800

    feat: Rust 程序设计语言 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rust-learning
+
+[Rust 程序设计语言](https://kaisery.github.io/trpl-zh-cn/title-page.html)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number");
+
+    let num = rand::thread_rng().gen_range(1..100);
+
+    loop {
+        println!("please input your guess");
+
+        //mut 使变量可变(Rust中，变量默认是不可变的)
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(..) => continue,
+        };
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&num) {
+            Ordering::Less => println!("small"),
+            Ordering::Equal => {
+                println!("win");
+                break;
+            }
+            Ordering::Greater => println!("big"),
+        }
+    }
 }
