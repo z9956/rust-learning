6ae367bae275f593e47b649c78e2260f7542968d
z9956 <zhz995617@gmail.com>
2021-09-29 17:59:36 +0800

    数据类型

---
+++ basis/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    //数值运算
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.2;
+    let product = 4 * 5;
+
+    //浮点型
+    // let x = 2.0;
+    // let y: f32 = 3.0;
+
+    /* //data types
+    let guess: u32 = "42".parse().expect("not a number");
+    println!("guess: {}", guess);*/
+
+    /*let mut x = 5;
+    println!("the value of x is: {}", x);
+
+    x = 6;
+    println!("the value of x is: {}", x);
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("the value is :{}", THREE_HOURS_IN_SECONDS);
+
+    let num = 5;
+    let num = num * 3;
+
+    //隐藏（Shadowing)
+    {
+        let num = num * 2;
+        println!("16 num: {}", num);
+    }
+
+    println!("The value of num is :{}", num);*/
+}
