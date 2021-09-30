6d4674b358aff44fb203e1a6b2b21440ce904483
z9956 <zhz995617@gmail.com>
2021-09-30 17:58:16 +0800

    数据类型

--- basis/src/main.rs
+++ basis/src/main.rs
@@ -1,8 +1,48 @@
+fn log(num: i32, str: char) {
+    println!("num is :{}, str is :{}", num, str);
+}
+
 fn main() {
+    //函数
+    log(452, '😻');
+
+    /*
+    复合类型
+    原生符合类型: tuple(元组) array(数组)
+    */
+    //tuple
+    /*let tup: (i32, f64, u8) = (500, 6.4, 1);
+    //模式匹配解构
+    let (x, _, _) = tup;
+    println!("the value of x is: {}", x);
+    //点号（.）
+    println!("tup 0 is: {}", tup.0);*/
+
+    /*
+    array
+    array是固定长度
+    */
+    /* let arr = [1, 2, 3, 4, 5, 6];
+    let list: [i32; 3] = [1, 2, 3];
+
+    //变量名为 a 的数组将包含 5 个元素，这些元素的值最初都将被设置为 3。这种写法与 let a = [3, 3, 3, 3, 3]; 效果相同
+    let data = [3; 2];
+
+    let first = data[0];*/
+
+    /*
+    字符类型
+    char类型由单引号指定
+    char类型的大小为四个字节
+    */
+    // let str = 'z';
+    // let cat = '😻';
+
     //数值运算
-    let sum = 5 + 10;
-    let difference = 95.5 - 4.2;
-    let product = 4 * 5;
+    // let sum = 5 + 10;
+    // let difference = 95.5 - 4.2;
+    // let product = 4 * 5;
+    // let remainder = 43 % 5;
 
     //浮点型
     // let x = 2.0;
