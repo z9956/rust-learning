4213b0bb09954baa78ffd60b4bad6b6d3deac052
z9956 <zhz995617@gmail.com>
2021-10-06 23:25:30 +0800

    clone

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut str = String::from("hello");
+    str.push_str(", world");
+
+    println!("{}", str);
+
+    //内存与分配
+    //克隆
+    let clone_str = str.clone();
+
+    println!("{}", clone_str);
 }
